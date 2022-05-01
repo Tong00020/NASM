191d1c49ce4670d4649645a7d3bd8487b6ac2b2e
Tong00020 <tongzhou2000@gmail.com>
2022-04-30 21:12:53 -0300

    vai

--- soma.asm
+++ soma.asm
@@ -3,12 +3,10 @@ global  sum
         section .text
 sum:
         xorpd   xmm0, xmm0              ; inicializa com 0
-        cmp     rsi, 0                  ; caso length = 0
-        je      done
+       
 next:
         addsd   xmm0, [rdi]             ; adiciona
         add     rdi, 8                  ; move to next array element
-        dec     rsi                     ; count down
+        dec     rsi                     ; contagem
         jnz     next                    ; testa de continua
-done:
         ret                             ; retorna valor
